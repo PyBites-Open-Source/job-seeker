7627448c792d2b85a2a4e77f131f4e857302d306
Bob Belderbos <bobbelderbos@gmail.com>
2021-10-02 12:51:06 +0200

    use vcr https://pytest-vcr.readthedocs.io/en/latest/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,57 +1,20 @@
-from unittest.mock import patch
-import json
-from pathlib import Path
+import os
 
 import pytest
 
 from job_seeker.downloader import JobSeeker
-import requests
 
-parent = (Path(__file__).absolute().parent)
 
-parameters = {
-    "where" : "All Adelaide SA",
-    "keywords" : "data analyst",
-}
+@pytest.fixture(scope="module")
+def vcr_cassette_dir(request):
+    return ""
 
-def mocked_requests_get(url: str, params: dict = None):
-    request_url = url
-    if params:
-        default_params = dict(
-            siteKey="AU-Main",
-            sourcesystem="houston",
-            page="1",
-            seekSelectAllPages="true",
-            )
-        params = dict(params, **default_params)
-        url += "?"
-        for k, v in params.items():
-            url += f"{k}={v}&"
 
-    url = url.strip("&")
-    url = "+".join(url.split(" "))
-
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-            self.ok = True if status_code == 200 else False
-
-        def json(self):
-            return self.json_data
-
-    if "https://www.seek.com.au/api/chalice-search/search" in url:
-        with open( parent / "data/response.json", "r+") as fp:
-            resp = json.load(fp)
-            
-            resp["page"] = params["page"]
-            
-        return MockResponse(resp, 200)
-
-    return MockResponse(None, 404)
-
-@pytest.fixture(scope="function")
-def js_instance():
-    with patch("requests.get", side_effect=mocked_requests_get):
-        js = JobSeeker(params=parameters)
-        yield js
+@pytest.fixture(scope="module")
+def js_instance(vcr):
+    params = {
+        "where": "All Adelaide SA",
+        "keywords": "data analyst",
+    }
+    with vcr.use_cassette("tests/cassettes/jobseeker.yaml"):
+        return JobSeeker(params=params)
--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -8,22 +8,22 @@ def test_params_dic(js_instance):
     assert isinstance(js_instance.params, dict)
 
 def test_total_count(js_instance):
-    assert js_instance.total_count == 320
+    assert js_instance.total_count == 604
 
 def test_get_total_count(js_instance):
-    total_count = js_instance._get_jobs_count() 
-    assert total_count == 320
+    total_count = js_instance._get_jobs_count()
+    assert total_count == 604
 
 def test_get_pages(js_instance):
-    total_count = js_instance._get_jobs_count() 
+    total_count = js_instance._get_jobs_count()
     pages = js_instance._total_pages(total_count)
-    assert pages == 16
+    assert pages == 30
 
 def test_df_instance(js_instance):
     df = js_instance.jobs_df
     assert isinstance(df, pd.DataFrame)
-    assert df.shape == (320, 14)
-    assert df["page"].max() == 16
+    assert df.shape == (658, 14)
+    assert df["page"].max() == 30
 
 def test_df_io_output(js_instance):
     io_output = js_instance.df_io
