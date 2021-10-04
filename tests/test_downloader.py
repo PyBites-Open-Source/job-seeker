38629e9e3c14ca2b650e670692853e1ded30930f
Pedro Junqueira <pedrocj@gmail.com>
2021-10-04 11:12:59 +1030

    updating tests

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -6,25 +6,25 @@ def test_params_dic(js_instance):
 
 
 def test_total_count(js_instance):
-    assert js_instance.total_count == 604
+    assert js_instance.total_count == 568
 
 
 def test_get_total_count(js_instance):
     total_count = js_instance._get_jobs_count()
-    assert total_count == 604
+    assert total_count == 568
 
 
 def test_get_pages(js_instance):
     total_count = js_instance._get_jobs_count()
     pages = js_instance._total_pages(total_count)
-    assert pages == 30
+    assert pages == 28
 
 
 def test_df_instance(js_instance):
     df = js_instance.jobs_df
     assert isinstance(df, pd.DataFrame)
-    assert df.shape == (658, 14)
-    assert df["page"].max() == 30
+    assert df.shape == (616, 14)
+    assert df["page"].max() == 28
 
 
 def test_df_io_output(js_instance):
